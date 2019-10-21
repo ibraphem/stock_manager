483274b7bcec8b75a8ee11b0699851c39f1932c1
yaqub <yaqub.adesola@gmail.com>
2019-10-21 13:13:38 +0100

    Re editing debts.blade.php properly to show Debtors instead of Customers

--- resources/views/customer/debts.blade.php
+++ resources/views/customer/debts.blade.php
@@ -4,7 +4,7 @@
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-      <h1>{{__('Customer')}}</h1>
+      <h1>{{__('Debtots')}}</h1>
     </section>
     <!-- Main content -->
     <section class="content">
